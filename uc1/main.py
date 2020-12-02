87456943af40f0429a39116e76e2b8f843a7dd97
Marko Rimac <mrimac97@gmail.com>
2020-12-02 22:28:10 +0100

    topology with modified AS graph generator.

--- uc1/main.py
+++ uc1/main.py
@@ -1,59 +1,57 @@
-from uc1_graph import Uc1_graph
 import networkx as nx
 import argparse
+import json
+from my_as_graph_gen import my_random_internet_as_graph
 
-def main(args):
+def main(data):
     """
     TOPOLOGY creation
     """
-    graph = Uc1_graph(args.core_node_count, args.gw_node_count, args.method, args.star_node_count, args.variance, args.seed)
-    nx.write_gexf(graph.G, args.outFILE + '.gexf')
+    G = my_random_internet_as_graph(data.nb_regions, data.nb_core_nodes, data.nb_gw_per_region, data.nb_gw_per_region_variance, data.avg_deg_core_node, data.nb_mm, data.nb_mm_variance, data.seed)
+    #graph = Uc1_graph(args.core_node_count, args.gw_node_count, args.method, args.star_node_count, args.variance, args.seed)
+    nx.write_gexf(G, data.outFILE + '.gexf')
 
 
-def check_bigger_than_three(value):
-    ival = int(value)
-    if ival < 3:
-        raise argparse.ArgumentTypeError("Number of nodes in core part has to be at least 3." +
-                                         "That is, it has to be one DC one NR and one GW")
-    return ival
-
-def check_bigger_than_one(value):
+def check_bigger_than_zero(value):
     ival = int(value)
     if ival < 1:
-        raise argparse.ArgumentTypeError("Number of GW nodes has to be greater than 1")
+        raise argparse.ArgumentTypeError("Has to be greater than 0")
     return ival
 
-def check_possible_methods(method):
-    if method not in core_graph_methods:
-        raise argparse.ArgumentTypeError("Not valid method! Valid methods are: " + str(core_graph_methods))
-    return method
+def check_positive_and_zero(value):
+    ival = int(value)
+    if ival < 0:
+        raise argparse.ArgumentTypeError("Has to be positive!")
+    return ival
 
-def get_and_check_args():
+def get_and_check_args(data):
     parser.add_argument("outFILE",
                         type=str,
                         help="output file name for generated graph")
-    parser.add_argument("core_node_count", type=check_bigger_than_three,
-                              help="Number of nodes in core part of topology")
-    parser.add_argument("gw_node_count", type=check_bigger_than_one, help="Number of GW nodes")
-    parser.add_argument("method", type=check_possible_methods, help="Name of method used to generate core function, possible:" + str(core_graph_methods))
-    parser.add_argument("star_node_count", type=check_bigger_than_one, help="middle val for normal distribution")
-    parser.add_argument("variance", type=int, help="variance for normal distribution")
-    parser.add_argument("seed", type=int, help="set seed for random values")
+    parser.add_argument("nb_regions", type=check_bigger_than_zero,
+                        help="Number of regions")
+    parser.add_argument("nb_core_nodes", type=check_bigger_than_zero, help="Number of core nodes")
+    parser.add_argument("nb_gw_per_region", type=check_positive_and_zero, help="Number of gateways per region")
+    parser.add_argument("nb_gw_per_region_variance", type=check_positive_and_zero, help="Number of gateways per region variance")
+    parser.add_argument("avg_deg_core_node", type=check_bigger_than_zero, help="average degree of a core node, Pick a random integer with uniform probability.")
+    parser.add_argument("nb_mm", type=check_positive_and_zero, help="Number of measuring modules")
+    parser.add_argument("nb_mm_variance", type=check_positive_and_zero, help="variance for normal distribution for number of measuring modules")
+    parser.add_argument("seed", type=check_positive_and_zero, help="set seed for random values")
 
-    arguments = parser.parse_args()
+    x = list(data.values())
+    arguments = parser.parse_args(x)
 
-    # Post parser conditions. Conditions between args checked.
-    if arguments.gw_node_count > arguments.core_node_count - 2:
-        raise argparse.ArgumentTypeError("Please make sure that: nb_gw <= nb_core_nodes - 2"
-                                         "always have to exist ")
+    # Ovisnosti među argumentima
+    if (arguments.nb_gw_per_region + arguments.nb_gw_per_region_variance * 3) * arguments.nb_regions > arguments.nb_core_nodes - arguments.nb_regions:
+        raise argparse.ArgumentTypeError("Please make sure that max possible number of gateways (considering 3sigma normal distribution with a variance) be less than a total nb_core_nodes! ")
     return arguments
 
 
 parser = argparse.ArgumentParser()
-core_graph_methods = ("newman-watts-strogatz", "barabasi-albert", "erdos-renyi", "euclidean")
 
 if __name__ == '__main__':
-    args = get_and_check_args()
+    data = json.load(open('topology_input.json'))
+    args = get_and_check_args(data)
     main(args)
 
 
